9e9f181e052797deea3ae9a13428b017b0e96619
ZeeshanJaved28 <zeeshanjaved28@hotmail.com>
2024-09-05 18:45:05 +0500

    Introduction-to-JavaScript

---
+++ Video_54/script54.js
@@ -0,0 +1,23 @@
+alert("Hello World");
+
+// console.log("Code is running...");
+// console.log("Code is also running...");
+// console.log("Code is running like a wow...");
+
+// console.error("");
+// console.warning("");
+
+var a = prompt("Enter your number: ");
+console.log("Your number is " + a);
+
+var isTrue = confirm(
+  "Are you sure you want leaving this page and blast your computer"
+);
+if (isTrue) {
+  console.log("Computer is blasting");
+} else {
+  console.log("Computer is not blasting");
+}
+
+document.title = "Hey I am good"
+// document.body.style.backgroundColor = "blue"
