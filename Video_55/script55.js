9e0b0ae3ee6b1e622845efd91eaee48939ef70c9
ZeeshanJaved28 <zeeshanjaved28@hotmail.com>
2024-09-06 22:18:55 +0500

    JavaScript-Variables-Data-Types-And-Object

---
+++ Video_55/script55.js
@@ -0,0 +1,56 @@
+console.log("Hey this is tutorial 55 of Javascript");
+
+// var a = 5;
+// a = a + 1;
+// var b = 6;
+// var c ="Zeeshan"
+// var _a = "Shani"
+// var 55a = "Zee" // Not Allowed
+
+// console.log(a+b);
+// console.log(a+b+8);
+// console.log(c);
+// console.log(typeof a, typeof b, typeof c);
+
+// const a1 = 6;
+// a1 = a1 + 1; // Not Allowed because a1 is constant
+
+let a = 5;
+// a = a + 1;
+let b = 6;
+let c = "Zeeshan";
+let _a = "Shani";
+
+{
+  // let a = 66
+  console.log(a);
+}
+console.log(a);
+
+// var a= 5;
+// {
+//     var a = 66
+//     console.log(a);
+// }
+// console.log(a);
+
+let x = "Shani Bhai";
+let y = 23;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r);
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r);
+
+let obj = {
+  name: "Zeeshan",
+  "job code": 5600,
+  is_handsome: true,
+};
+console.log(obj);
+o.salary = "100 Billion";
+console.log(obj);
+o.salary = "500 Billion";
+console.log(obj);
