f49ae1c7ee3460347fde6604ff68175d49af94a1
ZeeshanJaved28 <zeeshanjaved28@hotmail.com>
2024-09-08 00:44:36 +0500

    JavaScript-Loops

---
+++ Video_57/script57.js
@@ -0,0 +1,68 @@
+console.log("I'm a tutorial on Loops");
+
+// let a = 1;
+// console.log(a);
+// console.log(a + 1);
+// console.log(a + 2);
+
+// for (let i = 0; i < 100; i++) {
+//   console.log(a + i);
+// }
+
+/* Table of 2
+let b = 2;
+
+for (let i = 1; i<11; i++){
+    console.log(b*i)
+}
+*/
+
+// let obj = {
+//   name: "Shani",
+//   role: "Engineer",
+//   company: "Sigma AI",
+// };
+
+// for (const key in obj) {
+//   const element = obj[key];
+//   console.log(key, element);
+// }
+
+// for (const key in obj) {
+//   console.log(key);
+// }
+
+// for (const c of "Shani") {
+//   console.log(c);
+// }
+
+// let i = 5;
+// while(i<6){
+//     // 5<6 true and 6<6 false
+//     console.log(i)
+//     // print 5 and exit loop
+//     i++
+//     // 6
+// }
+
+// let i = 0;
+// while(i<6){
+//     console.log(i)
+//     i++ // if i not add this condition, it will turn into infinite loop.
+// }
+
+// let i = 0;
+//  //1st
+// do {
+//     console.log(i)
+//     // print 0  //2nd
+//     i++
+//     //4th
+// } while (i<6);
+// // 0 < 6 //3rd
+
+// let i = 10;
+// do {
+//     console.log(i)
+//     i++
+// } while (i<6);
