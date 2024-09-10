38bfe089ad13e916464f818b0e8f75881eb08417
ZeeshanJaved28 <zeeshanjaved28@hotmail.com>
2024-09-10 19:43:35 +0500

    Exercise-9-Faulty-Calculator

---
+++ Video_59/script59.js
@@ -0,0 +1,13 @@
+console.log("Exercise 9 - Faulty Calculator");
+
+/* Create a faulty calculator using javascript 
+
+This faulty calculator does following: 
+=> It takes two numbers as an input from the user.
+=>  It performs wrong operations as follows:
+(+) Addition --> (-) Subtraction
+(*) Multiplication --> (+) Addition
+(-) Subtraction --> (/) Division
+(/) Division --> (**) Exponentiation
+=> It performs wrong operation 10% of the times.
+*/
