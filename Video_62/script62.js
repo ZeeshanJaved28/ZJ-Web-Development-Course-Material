c72cb5d9e105e11684d1671673324dc4d92fa6f5
ZeeshanJaved28 <zeeshanjaved28@hotmail.com>
2024-09-13 19:05:28 +0500

    Exercise-10-Business-Name-Generator

---
+++ Video_62/script62.js
@@ -0,0 +1,23 @@
+console.log("JavaScript Exercise 10 - Business Name Generator");
+
+/* Create a business name generator by combining list of adjectives and shop
+name and another word
+
+Adjectives:
+Crazy
+Amazing
+Fire
+
+Shop Name:
+Engine
+Foods
+Garments
+
+Another Word:
+Bros
+Limited
+Hub
+
+Please Don't Use Arrays.  
+Use Strings , Functions.
+*/
