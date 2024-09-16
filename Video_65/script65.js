9dfeb9eaef2382de2d503da7423e17e136ff02ef
ZeeshanJaved28 <zeeshanjaved28@hotmail.com>
2024-09-16 17:37:03 +0500

    Exercise-11-Calculate-The-Factorial

---
+++ Video_65/script65.js
@@ -0,0 +1,7 @@
+console.log("JavaScript Exercise 11 - Calculate the Factorial");
+
+// Write a program to calculate the factorial of a number using reduce and using for loops.
+
+// lets suppose I giving you number is: 6
+
+// So factorial of a number 6 is 6*5*4*3*2 = 720
