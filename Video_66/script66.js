9bdf0037c3c52ce057e4ff9dbed6345350af81c8
ZeeshanJaved28 <zeeshanjaved28@hotmail.com>
2024-09-17 19:04:28 +0500

    Document-Object-Model-In-JavaScript

---
+++ Video_66/script66.js
@@ -0,0 +1,14 @@
+console.log("Document Object Model in JavaScript");
+
+document.title = "Shani is good";
+
+console.log(document.title);
+console.log(document.body);
+
+document.body.style.backgroundColor = "red";
+
+console.log(window);
+
+//console.log(window.document);
+
+//console.log(window.document.body);
