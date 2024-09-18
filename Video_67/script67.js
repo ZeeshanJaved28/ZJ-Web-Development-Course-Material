2124438231a0079132d99632cceaf7cc20e03aab
ZeeshanJaved28 <zeeshanjaved28@hotmail.com>
2024-09-18 18:24:06 +0500

    JS-DOM-Children_Parent-And-Sibling-Nodes

---
+++ Video_67/script67.js
@@ -0,0 +1,41 @@
+console.log("JavaScript DOM - Children, Parent & Sibling Nodes");
+
+console.log(document.body);
+
+// console.log(document.body.childNodes);
+// console.log(document.body.childNodes[0]);
+// console.log(document.body.childNodes[1]);
+// console.log(document.body.childNodes[1].childNodes);
+
+// let cont = document.body.childNodes[1];
+// console.log(cont);
+// console.log(cont.childNodes);
+// console.log(cont.firstChild);
+// console.log(cont.lastChild);
+
+// let cont = document.body.childNodes[1];
+// console.log(cont.firstElementChild);
+// console.log(cont.lastElementChild);
+// console.log(cont.lastElementChild.style.color="red");
+// console.log(cont.lastElementChild.style.backgroundColor="blue");
+
+// let cont = document.body.childNodes[1];
+// console.log(cont.lastElementChild.parentElement);
+
+// console.log(document.body.firstElementChild);
+// console.log(document.body.firstElementChild.childNodes);
+// console.log(document.body.firstElementChild.children); // Only HTML Elements Available.
+// console.log(document.body.firstElementChild.children[0]);
+// console.log(document.body.firstElementChild.children[1]);
+// console.log(document.body.firstElementChild.children[2]);
+// console.log(document.body.firstElementChild.children[3]);
+// console.log(document.body.firstElementChild.children[3].nextElementSibling);
+// console.log(document.body.firstElementChild.children[3].previousElementSibling);
+// console.log(document.body.firstElementChild.children[3].previousSibling);
+// console.log(document.body.firstElementChild.children[3].parentElement);
+
+console.log(document.body.children);
+console.log(document.body.children[1]);
+console.log(document.body.children[1].rows);
+console.log(document.body.children[1].columns);
+console.log(document.body.children[1].tBodies);
