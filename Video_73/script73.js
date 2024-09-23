c4b257234187c425c8ceb08dacd2f8ee5e152822
ZeeshanJaved28 <zeeshanjaved28@hotmail.com>
2024-09-23 19:23:36 +0500

    Exercise-13-Dynamic-Website-Builder

---
+++ Video_73/script73.js
@@ -0,0 +1,14 @@
+console.log("JavaScript Exercise 13 - Dynamic Website Builder");
+
+function createCard(title, cName, views, monthsOld, duration, thumbnail) {
+  // Finish this function
+}
+
+createCard(
+  "Introduction to Web Development | ZJ Web Dev topic # 02",
+  "CodeWithZJ",
+  650000,
+  3,
+  "30:00",
+  "https://i.ytimg.com/vi/KtL-SQ20Q0s/hqdefault.jpg?sqp=-oaymwEbCKgBEF5IVfKriqkDDggBFQAAiEIYAXABwAEG&rs=AOn4CLABJhdIBqlilwXKzJqa6StynbJT4Q"
+);
