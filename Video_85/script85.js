48c3080babda7685dc49eeedf22062ee2eded906
ZeeshanJaved28 <zeeshanjaved28@hotmail.com>
2024-10-09 22:36:59 +0500

    Backend-NodeJS-And-NPM

---
+++ Video_85/script85.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+
+var slugify = require("slugify");
+let a = slugify('some string') // some-string
+console.log(a);
+
+// if you prefer something other than '-' as separator
+let b = slugify('some string','_') // some_string
+console.log(b);
+
+const c = slugify('some st&&*(^%$$^^&ring','_') // some_string
+console.log(c);
