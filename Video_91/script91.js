3e82150e938e68ffd396af36d2a3582836513310
ZeeshanJaved28 <zeeshanjaved28@hotmail.com>
2024-10-16 23:52:18 +0500

    Exercise-15-Clear-The-Clutter

---
+++ Video_91/script91.js
@@ -0,0 +1,18 @@
+console.log("Exercise 15 - Clear the Clutter");
+
+// You have to write a Node.js program to clear the clutter inside of a directory and organize the contents of that directory into different folders
+
+// for example, these files become:
+// 1. name.jpg
+// 2. name.png
+// 3. this.pdf
+// 4. shani.zip
+// 5. zee.zip
+// 6. cat.jpg
+// 7. shani.pdf
+
+// this:
+// jpg/name.jpg, jpg/catch.jpg
+// png/name.png
+// pdf/this.pdf, pdf/shani.pdf
+// zip/shani.zip, zip/zee.zip
