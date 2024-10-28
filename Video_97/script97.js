a3274952b37eb59706a08d98c1cdc142639cb556
ZeeshanJaved28 <zeeshanjaved28@hotmail.com>
2024-10-28 17:43:48 +0500

    Exercise-16-Dummy-Data-Generator

---
+++ Video_97/script97.js
@@ -0,0 +1,17 @@
+console.log('Exercise 16 - Dummy Data Generator');
+
+// Generate a dummy data in this format in a collection called Employees in a db called Company.
+
+// {
+//     name: "Shani",
+//     salary:45000000,
+//     language: "Python",
+//     city: "Khi",
+//     isManager: true
+// }
+
+// Generate 10 such records when a buttton called generate data is clicked!
+
+// Create an Express App with mongoose to achieve it.
+
+// Every time the button is clicked , you should clear the collection.
